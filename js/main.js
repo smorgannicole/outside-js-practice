81cc0d41742b23f4d30db80197153a735515f8d7
Morgan <smorgannicole@gmail.com>
2023-11-16 08:24:19 -0600

    card, add your own section

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 import { debounce } from './utils.js';
 const coffees = [
-	{ id: 1, name: "Light City", roast: "light" },
-	{ id: 2, name: "Half City", roast: "light" },
-	{ id: 3, name: "Cinnamon", roast: "light" },
-	{ id: 4, name: "City", roast: "medium" },
-	{ id: 5, name: "American", roast: "medium" },
-	{ id: 6, name: "Breakfast", roast: "medium" },
-	{ id: 7, name: "High", roast: "dark" },
-	{ id: 8, name: "Continental", roast: "dark" },
-	{ id: 9, name: "New Orleans", roast: "dark" },
-	{ id: 10, name: "European", roast: "dark" },
-	{ id: 11, name: "Espresso", roast: "dark" },
-	{ id: 12, name: "Viennese", roast: "dark" },
-	{ id: 13, name: "Italian", roast: "dark" },
-	{ id: 14, name: "French", roast: "dark" },
+	{ id: 1, name: "Galactic Dawn", roast: "light" },
+	{ id: 2, name: "Nebula Noir", roast: "light" },
+	{ id: 3, name: "Stellar Symphony", roast: "light" },
+	{ id: 4, name: "Aurora Elation", roast: "medium" },
+	{ id: 5, name: "Celestial Ember", roast: "medium" },
+	{ id: 6, name: "Lunar Eclipse", roast: "medium" },
+	{ id: 7, name: "Supernova Spice", roast: "dark" },
+	{ id: 8, name: "Cosmic Caramel Comet", roast: "dark" },
+	{ id: 9, name: "Solar Flare Fusion", roast: "dark" },
+	{ id: 10, name: "Interstellar Infusion", roast: "dark" },
+	{ id: 11, name: "Andromeda Awakening", roast: "dark" },
+	{ id: 12, name: "Cosmic Cocoa Cloud", roast: "dark" },
+	{ id: 13, name: "Starlight Serenade", roast: "dark" },
+	{ id: 14, name: "Cosmic Origin Odyssey", roast: "dark" },
 ];
 
 const addCoffee = (coffeeName, roastType) => {
@@ -33,11 +33,12 @@ const renderCoffeeElement = (coffee) => {
 	const coffeeElement = document.createElement("div");
 	coffeeElement.classList.add(`d-flex`)
 	coffeeElement.innerHTML = `
-		<div class="col">
-			<p>${coffee.name}</p>
-		</div>
-		<div class="col">
-			<p>${coffee.roast}</p>
+		<div class="card" style="width: 18rem;">
+  			<div class="card-body">
+				<h5 class="card-title">${coffee.name}</h5>
+				<h6 class="card-subtitle mb-2 text-body-secondary">${coffee.roast}</h6>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  			</div>
 		</div>
 		${coffee.userGenerated ? `<button class="btn btn-danger" data-delete>Delete</button>` : ``}
 		${coffee.userGenerated ? `<button class="btn btn-secondary" data-edit>Edit</button>` : ``}
