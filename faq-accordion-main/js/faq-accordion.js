ba1f0153933a7875674ef539a89d2e4746d8292e
Morgan <smorgannicole@gmail.com>
2023-12-09 19:36:21 -0600

    got buttons linked to correct element/attribute

--- faq-accordion-main/js/faq-accordion.js
+++ faq-accordion-main/js/faq-accordion.js
@@ -1,16 +1,17 @@
 
 const expandEventListener = () => {
     const buttons = document.querySelectorAll(`.expandWhenClicked`);
-    for (let i = 0; i < buttons; i++) {
-        tabLinks[i].addEventListener('click', e => {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', e => {
             const tabId = buttons[i].getAttribute('data-plus');
-            for (let content of buttons) {
-                content.classList.remove('active');
-            }
-            const selectedTab = document.querySelector(`#${tabId}`);
-            if (selectedTab) {
-                selectedTab.classList.add('active');
-            }
+            console.log(tabId)
+            // for (let content of buttons) {
+            //     content.classList.remove('active');
+            // }
+            // const selectedTab = document.querySelector(`${tabId}`);
+            // if (selectedTab) {
+            //     selectedTab.classList.add('active');
+            // }
         });
     }
 }
