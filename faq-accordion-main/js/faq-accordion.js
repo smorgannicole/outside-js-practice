c193988f73682be24f632c5f3adec7eaec90c391
Morgan <smorgannicole@gmail.com>
2023-12-10 16:10:49 -0600

    finished this frontend mentor proj and got it looking mint

--- faq-accordion-main/js/faq-accordion.js
+++ faq-accordion-main/js/faq-accordion.js
@@ -1,19 +1,33 @@
 
 const expandEventListener = () => {
     const buttons = document.querySelectorAll(`.expandWhenClicked`);
-    for (let button of buttons) {
-        button.addEventListener(`click`, e => {
+    buttons.forEach(button => {
+        button.addEventListener(`click`, () => {
             const buttonId = button.getAttribute(`data-plus`);
-            const selectedButton = document.querySelector(`[data-plus="${buttonId}"]`);
-            const selectedQuestion = button.querySelector('.question');
+            const selectedQuestion = button.querySelector(`.question`);
             const selectedAnswer = document.querySelector(`[data-answer="${buttonId}"]`);
-            const selectedCircle = document.querySelector(`[data-circle="${buttonId}"]`)
-            selectedQuestion.classList.toggle(`active`);
-            selectedCircle.classList.toggle(`btnActive`);
-            selectedAnswer.classList.toggle(`hidden`);
-            selectedAnswer.classList.toggle('fade-in');
+            const selectedCircle = document.querySelector(`[data-circle="${buttonId}"]`);
+            const isActive = selectedQuestion.classList.contains(`active`);
+
+            buttons.forEach(btn => {
+                const question = btn.querySelector(`.question`);
+                const circle = document.querySelector(`[data-circle="${btn.getAttribute(`data-plus`)}"]`);
+                const answer = document.querySelector(`[data-answer="${btn.getAttribute(`data-plus`)}"]`);
+
+                if (question.classList.contains(`active`)) {
+                    question.classList.toggle(`active`);
+                    circle.classList.toggle(`btnActive`);
+                    answer.classList.toggle(`hidden`);
+                }
+            });
+
+            if (!isActive) {
+                selectedQuestion.classList.toggle(`active`);
+                selectedCircle.classList.toggle(`btnActive`);
+                selectedAnswer.classList.toggle(`hidden`);
+            }
         });
-    }
+    });
 }
 
 
