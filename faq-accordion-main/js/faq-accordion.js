0e1743025ca689510c5a89ef043c94bf924e737f
Morgan <smorgannicole@gmail.com>
2023-12-10 15:31:46 -0600

    added animations and answers to each faq

--- faq-accordion-main/js/faq-accordion.js
+++ faq-accordion-main/js/faq-accordion.js
@@ -1,17 +1,17 @@
 
 const expandEventListener = () => {
     const buttons = document.querySelectorAll(`.expandWhenClicked`);
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', e => {
-            const tabId = buttons[i].getAttribute('data-plus');
-            console.log(tabId)
-            // for (let content of buttons) {
-            //     content.classList.remove('active');
-            // }
-            // const selectedTab = document.querySelector(`${tabId}`);
-            // if (selectedTab) {
-            //     selectedTab.classList.add('active');
-            // }
+    for (let button of buttons) {
+        button.addEventListener(`click`, e => {
+            const buttonId = button.getAttribute(`data-plus`);
+            const selectedButton = document.querySelector(`[data-plus="${buttonId}"]`);
+            const selectedQuestion = button.querySelector('.question');
+            const selectedAnswer = document.querySelector(`[data-answer="${buttonId}"]`);
+            const selectedCircle = document.querySelector(`[data-circle="${buttonId}"]`)
+            selectedQuestion.classList.toggle(`active`);
+            selectedCircle.classList.toggle(`btnActive`);
+            selectedAnswer.classList.toggle(`hidden`);
+            selectedAnswer.classList.toggle('fade-in');
         });
     }
 }
